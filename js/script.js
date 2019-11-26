98db07c09ea570d0f6bd95cb8e27ec3b0dea171d
Sergey <Damastier@yandex.ru>
2019-11-26 21:00:07 +0300

    new start

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.carousel_inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-double-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-double-right"></i></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                }
+            }
+        ]
+    });    
+});
